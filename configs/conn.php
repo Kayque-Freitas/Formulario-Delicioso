bc9daca7ce5989fe0dae6b30b0abd4d37ec1a370
Kayque Freitas <66932883+Kayque-Freitas@users.noreply.github.com>
2025-10-07 21:40:25 -0300

    subindo pra hospedagem

--- configs/conn.php
+++ configs/conn.php
@@ -1,8 +1,8 @@
 <?php
-$host = "127.0.0.1:3306";
-$user = "root"; // usuário padrão do Laragon
-$pass = "";     // senha marota
-$db   = "kayqueFreitas"; // nome do banco de dados
+$host = "sql308.infinityfree.com";
+$user = "if0_39645844"; // usuário padrão do Laragon
+$pass = "208KqnHO2d";     // senha marota
+$db   = "if0_39645844_noite2"; // nome do banco de dados
 
 // Criando conexão
 $conn = new mysqli($host, $user, $pass, $db);
